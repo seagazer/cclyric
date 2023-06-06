b59559c2b2c35247897943aed17ae3f998e6bae6
seagazer <seagazer@qq.com>
2023-06-06 23:08:42 +0800

    [dev]增加解析歌词id和tag能力

--- cclyric/src/main/ets/parse/LyricParser.ts
+++ cclyric/src/main/ets/parse/LyricParser.ts
@@ -2,40 +2,80 @@ import { IParser } from './IParser';
 import { Lyric } from '../bean/Lyric';
 import { LyricLine } from '../bean/LyricLine';
 
+/**
+ * The parser to parse the string array of a standard lyric file.
+ */
 export class LyricParser implements IParser {
-    private parseTime(line: string): number {
-        let spr = line.split(']')
-        // let text = spr[1]
-        let spr1 = spr[0]
-        let spr1_1 = spr1.replace('[', "") //00:00.50
-        let spr2 = spr1_1.split(':')
-        let spr2_1 = spr2[0] //00
-        let minute = Number.parseInt(spr2_1)
-        let spr2_2 = spr2[1] //00.50
-        let spr3 = spr2_2.split(".")
-        let spr3_1 = spr3[0] //00
-        let spr3_2 = spr3[1] //50
-        let seconds = Number.parseInt(spr3_1)
-        let millionSecond = Number.parseInt(spr3_2)
-        return minute * 60000 + seconds * 1000 + millionSecond
-    }
+    // Example lyric file:
+    // [ti:画心]
+    // [ar:张靓颖]
+    // [al:432326]
+    // [by:]
+    // [offset:0]
+    // [00:00.10]画心 - 张靓颖
+    // [01:05.49]看不穿 是你失落的魂魄
 
+    /**
+     * Parse the string array to a Lyric.
+     * @param src The lyric source, a string array.
+     * @returns A lyric instance.
+     */
     parse(src: Array<string>): Lyric {
-        let list = new Array<LyricLine>()
+        let lyricLines = new Array<LyricLine>()
         let end = -1
+        let title = ""
+        let artist = ""
+        let album = ""
+        let by = ""
+        let offset = 0
         for (let i = 0;i < src.length; i++) {
             let line = src[i]
-            let spr = line.split(']')
-            let text = spr[1]
-            if (i < src.length - 1) {
-                let begin = end == -1 ? this.parseTime(line) : end
-                let nextLine = src[i + 1]
-                end = this.parseTime(nextLine)
-                list.push(new LyricLine(text, begin, end))
+            if (line.indexOf("ti") > 0) {
+                title = this.parseIdTag(line)
+            } else if (line.indexOf("ar") > 0) {
+                artist = this.parseIdTag(line)
+            } else if (line.indexOf("al") > 0) {
+                album = this.parseIdTag(line)
+            } else if (line.indexOf("by") > 0) {
+                by = this.parseIdTag(line)
+            } else if (line.indexOf("offset") > 0) {
+                offset = Number.parseInt(this.parseIdTag(line))
             } else {
-                list.push(new LyricLine(text, end, end + 1000))
+                let spr = line.split(']')
+                let text = spr[1] // text
+                if (i < src.length - 1) {
+                    let begin = end == -1 ? this.parseTimeline(line) : end
+                    let nextLine = src[i + 1]
+                    end = this.parseTimeline(nextLine)
+                    lyricLines.push(new LyricLine(text, begin - offset, end - offset))
+                } else {
+                    lyricLines.push(new LyricLine(text, end - offset, end + 1000 - offset))
+                }
             }
         }
-        return new Lyric(list)
+        return new Lyric(artist, title, album, by, offset, lyricLines)
+    }
+
+    private parseIdTag(line: string): string {
+        let spr = line.split(":")
+        let spr1 = spr[1]
+        let result = spr1.replace("]", "")
+        return result
+    }
+
+    private parseTimeline(line: string): number {
+        let spr = line.split(']') // timestamp
+        let time = spr[0]
+        let timeString = time.replace('[', "") //00:00.50
+        let timeStringList = timeString.split(':')
+        let minuteString = timeStringList[0] //00
+        let minute = Number.parseInt(minuteString)
+        let secondStrings = timeStringList[1] //00.50
+        let secondStringList = secondStrings.split(".")
+        let secondString = secondStringList[0] //00
+        let millionSecondString = secondStringList[1] //50
+        let seconds = Number.parseInt(secondString)
+        let millionSecond = Number.parseInt(millionSecondString)
+        return minute * 60000 + seconds * 1000 + millionSecond // covert to million seconds
     }
 }
